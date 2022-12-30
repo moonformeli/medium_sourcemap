6e85a5e76fd25bae58238648f1a19cd0971ca21a
moonformeli <mgyang95@gmail.com>
2022-12-31 04:19:17 +0900

    :recycle: uglify code in production build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,10 @@
 const { merge } = require('webpack-merge');
 const devConfig = require('./webpack.dev.config');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(devConfig, {
   mode: 'production',
+  optimization: {
+    minimizer: [new UglifyJsPlugin({})],
+  },
 });
