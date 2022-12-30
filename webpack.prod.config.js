f329aafd936115528ea5eff732830d59e8f9b848
moonformeli <mgyang95@gmail.com>
2022-12-31 00:44:26 +0900

    :sparkles: set default project environment

---
+++ webpack.prod.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'hello_world.js'),
+  output: {
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+};
